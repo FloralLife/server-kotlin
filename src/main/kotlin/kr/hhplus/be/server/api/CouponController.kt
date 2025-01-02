4db320996944972aa31c3856665d350923718a27
yunoh-lee <yunoh.lee@linecorp.com>
2025-01-03 03:41:06 +0900

    ✨ feat : Coupon Mock API 구현

--- src/main/kotlin/kr/hhplus/be/server/api/CouponController.kt
+++ src/main/kotlin/kr/hhplus/be/server/api/CouponController.kt
@@ -1,9 +1,19 @@
 package kr.hhplus.be.server.api
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/coupons")
 class CouponController {
+  class Coupon (
+    val id: Long,
+    val name: String,
+    val discountRate: Int,
+    var stock: Int,
+    val expirationPeriod: Int
+  )
+
+  val coupon = Coupon(1L, "선착순 쿠폰", 1, 100, 90)
 }
--- src/main/kotlin/kr/hhplus/be/server/api/UserController.kt
+++ src/main/kotlin/kr/hhplus/be/server/api/UserController.kt
@@ -3,10 +3,12 @@ package kr.hhplus.be.server.api
 import kr.hhplus.be.server.api.request.PointChargeRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/users")
@@ -16,13 +18,33 @@ class UserController {
     var point: Int
   )
 
+  class UserCoupon(
+    val id: Long,
+    val couponId: Long,
+    val expirationDate: LocalDate,
+    val userId: Long,
+    var orderId: Long?
+  )
+
   val user: User = User(1L, 50_000_000)
+  val userCoupon = UserCoupon(1L, 1L, LocalDate.MAX, 1L, null)
 
   @GetMapping("/{userId}")
   fun get(@PathVariable userId: Long): User {
     require(userId > 0L) { "유저 id는 양수입니다." }
     require(userId == 1L) { "존재하지 않습니다." }
     return user
+  }
+
+  @PostMapping("/{userId}/coupons/{couponId}")
+  fun registerCoupon(@PathVariable userId: Long, @PathVariable couponId: Long): UserCoupon {
+    require(userId > 0L) { "유저 id는 양수입니다." }
+    require(userId == 1L) { "존재하지 않습니다." }
+
+    require(couponId > 0L) { "쿠폰 id는 양수입니다." }
+    require(couponId == 1L) { "존재하지 않습니다." }
+
+    return userCoupon
   }
 
   @GetMapping("/{userId}/point")
